08b0d41f0abba667946a099ae81a12d74b026b9b
austinnebel <nebelaustin@gmail.com>
2021-09-15 15:27:24 -0500

    Added spray_times to humidifier

--- devices/humidifier.py
+++ devices/humidifier.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import datetime as dt
 from time import sleep
 from threading import Thread
 from .relay import Relay
@@ -9,6 +10,25 @@ class Humidifier:
 
     def __init__(self, pin):
         self.relay = Relay(pin)
+        self.spray_times = []
+
+    def add_spray(self):
+        now = dt.now()
+        if len(self.spray_times) >= 0:
+            for t in self.spray_times:
+                if (now-t).total_seconds() > 120*60:
+                    self.spray_times.remove(t)
+        self.spray_times.append(now)
+
+    def last_spray(self):
+        """
+        Returns last recorded spray time.
+
+        Returns:
+            datetime: Last spray time.
+        """
+        if len(self.spray_times) >= 0:
+            return self.spray_times[-1]
 
     def is_on(self):
         return self.relay.is_on()
@@ -29,7 +49,10 @@ class Humidifier:
         self.off()
 
     def spray(self, spray_time):
-        print(f"Spraying humidifier for {spray_time} seconds.")
+        """
+        Sprays the humidifer for spray_time seconds.
+        """
+        LOGGER.info(f"Spraying humidifier for {spray_time} seconds.")
         t = Thread(target = self._spray, args = [spray_time])
         t.start()
-
+        self.add_spray()
