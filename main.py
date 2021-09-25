bfde2e5a8e86cb2574885d7240cacab798554920
austinnebel <nebelaustin@gmail.com>
2021-09-25 14:20:50 -0500

    Added heat lamp device

--- main.py
+++ main.py
@@ -66,6 +66,11 @@ def main(config):
     TEMP_RANGE = config.getint("THERMOSTAT", "temp_range")
     HUM_RANGE = config.getint("THERMOSTAT", "humidity_range")
     BUFFER_DUR = config.getint("THERMOSTAT", "buffer_duration")
+    SPRAY_DUR = config.getint("THERMOSTAT", "spray_duration")
+
+    # schedule settings
+    DAY_START = config.getint("SCHEDULE", "day_start")
+    DAY_END = config.getint("SCHEDULE", "day_end")
 
     # server settings
     PORT = config.getint("SERVER", "port")
@@ -74,10 +79,12 @@ def main(config):
     # gpio settings
     HEATER_GPIO = config.getint("GPIO", "heater")
     HUMIDITY_GPIO = config.getint("GPIO", "humidifier")
+    LAMP_GPIO = config.getint("GPIO", "lamp")
     DHT_GPIO = config.getint("GPIO", "dht22")
 
     # initialize devices
-    HEATER = Heater(HEATER_GPIO)
+    HEATER = Heater(HEATER_GPIO, name = "Heating Pad", normally_closed = True)
+    LAMP = Heater(LAMP_GPIO, name = "Lamp", normally_closed = False)
     HUMIDIFIER = Humidifier(HUMIDITY_GPIO, GRAPH_DURATION)
     DHT = TempSensor(DHT_GPIO, buffer_duration=BUFFER_DUR)
 
@@ -85,6 +92,7 @@ def main(config):
         LOGGER.info("Exiting Session")
         if HEATER: HEATER.on()
         if HUMIDIFIER: HUMIDIFIER.off()
+        if LAMP: LAMP.off()
         if DHT: DHT.terminate()
         TERM.set()
     signal.signal(signal.SIGINT, exit_handler)
@@ -126,21 +134,38 @@ def main(config):
             plot_points.pop(0)
         plot_points.append(reading)
 
-        generate_graphs(plot_points, HUMIDIFIER.spray_times, GRAPH_LOCATION)
+        # generate new graphs, and updated HTML page
+        generate_graphs(plot_points, HUMIDIFIER.get_spray_times(), GRAPH_LOCATION)
+
+
+        current_hour = datetime.datetime.now().hour
 
         # run thermostat checks
+        is_daytime = current_hour > DAY_START and current_hour < DAY_END
+        print(temp)
+        print(DESIRED_TEMP)
+        print(TEMP_RANGE)
+        print(temp < DESIRED_TEMP-TEMP_RANGE)
+        print(is_daytime)
+        print(current_hour)
         if temp < DESIRED_TEMP-TEMP_RANGE:
-            HEATER.on()
+            # only use heat lamp during daytime hours. If not daytime, use heat pad
+            if is_daytime:
+                LAMP.on()
+            else:
+                LAMP.off()
+                HEATER.on()
         if temp > DESIRED_TEMP+TEMP_RANGE:
             HEATER.off()
+            LAMP.off()
 
         if hum < DESIRED_HUM-HUM_RANGE and not hum < 0 and not hum > 100:
-            HUMIDIFIER.spray(15)
+            HUMIDIFIER.spray(SPRAY_DUR)
 
         # wait UPDATE_TIME seconds, subtracting execution time of loop
         TERM.wait(UPDATE_TIME-(time.time()-s))
 
-    LOGGER.info("Exit successfull.")
+    LOGGER.info("Exited main loop.")
 
 if __name__ == "__main__":
     config = configparser.ConfigParser()
