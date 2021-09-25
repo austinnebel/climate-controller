4b758b9755e09f0f9c6c880c26fdc2ffe4cc33e5
austinnebel <nebelaustin@gmail.com>
2021-09-25 14:18:50 -0500

    Added support for multiple heating devices

--- devices/heater.py
+++ devices/heater.py
@@ -5,18 +5,28 @@ LOGGER = logging.getLogger()
 
 class Heater:
 
-    def __init__(self, pin):
-        self.relay = Relay(pin)
+    def __init__(self, pin, name = "Heater", normally_closed = True):
+        """
+        Controls a heating device.
+
+        Args:
+            pin (int): GPIO pin to activate the heater.
+            normally_closed (bool, optional): If True, the relay for this device is normally closed i.e. turns off when its GPIO is activated.
+                                              If False, the relay for this device is normally open i.e. turns on when its GPIO is activated.
+                                              Defaults to True.
+        """
+        self.relay = Relay(pin, normally_closed = normally_closed)
+        self.name = name
 
     def is_on(self):
-        return not self.relay.is_on()
+        return self.relay.is_on()
 
     def on(self):
         if not self.is_on():
-            LOGGER.info("Activating heater.")
-            self.relay.off()
+            LOGGER.info(f"Activating {self.name}.")
+            self.relay.on()
 
     def off(self):
         if self.is_on():
-            LOGGER.info("Deactivating heater.")
-            self.relay.on()
+            LOGGER.info(f"Deactivating {self.name}.")
+            self.relay.off()
