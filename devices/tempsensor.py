c2ea68808a29138766843c4613b864d7c689a4b6
austinnebel <nebelaustin@gmail.com>
2021-09-25 14:20:07 -0500

    Imporved tempsensor docstrings, added clean_buffer method

--- devices/tempsensor.py
+++ devices/tempsensor.py
@@ -9,25 +9,23 @@ LOGGER = logging.getLogger()
 
 class Reading:
 
-    def __init__(self, temp, hum, convert = True, use_fahrenheit = True):
+    def __init__(self, temp, hum, time = None, convert = True, repr_fahrenheit = True):
         """
          Holds temperature and humidity data, as well as the time of data capture.
-
-         Automatically will make conversions from Celcius to Fahrenheit if `convert` is true.
-
-         NOTE: use_fahrenheit is only used for logging output.
-
 
         Args:
             temp (float): Temperature.
             hum (float): Humidity percentage.
-            convert (bool, optional): If true, makes conversion to fahrenheit if use_fahrenheit is True. Defaults to True.
-            use_fahrenheit (bool, optional): Whether to use fahrenheit units in logging output. Defaults to True.
+            time (datetime): If present, will set this objects timestamp.
+            convert (bool): If True, will convert temp from Celcius to Fahrenheit.
+            repr_fahrenheit (bool, optional): If True and convert is False, assumes that temp is already in Fahrenheit units.
+                                              If True and convert is True, temp will be converted to Fahrenheit units.
+                                              If False, temp will remain in Celcius.
         """
-        self.use_fahrenheit = use_fahrenheit
+        self.repr_fahrenheit = repr_fahrenheit
 
         if temp != None:
-            if self.use_fahrenheit and convert == True:
+            if convert and self.repr_fahrenheit:
                 temp = (temp * 9/5) + 32
             temp = round(temp, 2)
         if hum != None:
@@ -35,14 +33,13 @@ class Reading:
 
         self.temp = temp
         self.hum = hum
-        self.time = datetime.datetime.now()
+        self.time = datetime.datetime.now() if time is None else time
 
     def __str__(self):
         if self.temp == None or self.hum == None:
             return "Read error."
-        unit = "C"
-        if self.use_fahrenheit:
-            unit = "F"
+
+        unit = "F" if self.repr_fahrenheit else "C"
         return f"{self.temp}°{unit} {self.hum}% {self.time.strftime('%H:%M:%S')}"
 
 class TempSensor(Thread):
@@ -68,33 +65,10 @@ class TempSensor(Thread):
         self.term = Event()
 
     @property
-    def temperature(self):
-        """
-        Returns the latest recorded temperature.
-
-        Returns:
-            float: Last recorded temperature.
-        """
-        if len(self.reading_buff) > 0:
-            return self.reading_buff[-1].temp
-        return None
-
-    @property
-    def humidity(self):
-        """
-        Returns the latest recorded humidity percentage.
-
-        Returns:
-            float: Last recorded humidity percentage.
-        """
-        if len(self.reading_buff) > 0:
-            return self.reading_buff[-1].hum
-        return None
-
-    @property
     def avg(self):
         """
         Returns a `Reading` object containing the average values of all Readings stored in the buffer.
+        The returned time value will be the time value stored in the most recent reading.
 
         Returns:
             Reading, None: Reading object if readings are available in the buffer. False otherwise.
@@ -103,33 +77,41 @@ class TempSensor(Thread):
             t_avg = sum([r.temp for r in self.reading_buff])/len(self.reading_buff)
             h_avg = sum([r.hum for r in self.reading_buff])/len(self.reading_buff)
 
-            return Reading(t_avg, h_avg,  self.reading_buff[-1].time)
+            return Reading(t_avg, h_avg, time = self.reading_buff[-1].time, convert = False, repr_fahrenheit = self.use_fahrenheit)
         return None
 
+    def clean_buffer(self):
+        """
+        Removes all readings that are older than self.buffer_duration seconds.
+        """
+        if len(self.reading_buff) > 0:
+            now = datetime.datetime.now()
+            for r in self.reading_buff:
+                if (now - r.time).total_seconds() > self.buffer_duration:
+                    self.reading_buff.remove(r)
     @property
     def read(self):
         """
+        Returns the latest-recorded reading.
+
+        Returns:
             Reading: Latest recorded reading. None if unavailable.
         """
         if len(self.reading_buff) > 0:
+            self.clean_buffer()
             return self.reading_buff[-1]
         return None
 
-    @read.setter
-    def read(self, reading : Reading):
+    def add_reading(self, reading : Reading):
         """
-        Adds a Reading object to the reading buffer. Removes all readings
-        that are older than self.buffer_duration seconds.
+        Adds a Reading object to the reading buffer.
 
         Args:
             reading (Reading): Reading object captured from sensor.
         """
         if reading.temp is None or reading.hum is None: return
-        if len(self.reading_buff) >= 0:
-            for r in self.reading_buff:
-                if (reading.time - r.time).total_seconds() > self.buffer_duration:
-                    self.reading_buff.remove(r)
         self.reading_buff.append(reading)
+        self.clean_buffer()
 
     def get_readings(self):
         """
@@ -156,7 +138,7 @@ class TempSensor(Thread):
         humidity, temperature = Adafruit_DHT.read_retry(self.sensor, self.pin)
         if humidity != None: humidity = round(humidity, 2)
         if temperature != None: temperature = round(temperature, 2)
-        return Reading(temperature, humidity, use_fahrenheit=self.use_fahrenheit)
+        return Reading(temperature, humidity, convert = self.use_fahrenheit, repr_fahrenheit = self.use_fahrenheit)
 
     def run(self):
         """
@@ -165,7 +147,7 @@ class TempSensor(Thread):
         """
         while not self.term.is_set():
             try:
-                self.read = self.get_data()
+                self.add_reading(self.get_data())
             except Exception as e:
                 LOGGER.error(f"Error reading from DHT22: {str(e)}")
             # dht sensors need a minimum of 2 seconds between readings
