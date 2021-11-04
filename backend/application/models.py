d5ad7c6697aad20791fa20024438d731e1a9ab90
austinnebel <austin@true-360.com>
2021-11-04 14:17:15 -0500

    Added validators and constraints

--- backend/application/models.py
+++ backend/application/models.py
@@ -1,6 +1,16 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db.models import CheckConstraint, Q
 
 class ClimateData(models.Model):
-    temperature = models.FloatField(max_length=6)
-    humidity = models.FloatField(max_length=6)
-    time = models.TimeField(auto_now = True)
+    temperature = models.FloatField(max_length=6, blank=False)
+    humidity = models.FloatField(max_length=6, blank=False, validators=[MinValueValidator(0.0), MaxValueValidator(100.0)])
+    time = models.TimeField(auto_now = True, blank=False)
+
+    class Meta:
+        constraints = (
+            # validates humidity
+            CheckConstraint(
+                check=Q(humidity__gte=0.0) & Q(humidity__lte=100.0),
+                name='humidity_range'),
+            )
