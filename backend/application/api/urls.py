8adbb2c5799d4336c108f4334c6117a47f9681db
austinnebel <austin@true-360.com>
2021-11-04 14:17:47 -0500

    Simplified url configuration

--- backend/application/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from django.urls import path, include
-from .views import ClimateDataAPI
-
-urlpatterns = [
-    path('', ClimateDataAPI.as_view()),
-]
--- backend/application/urls.py
+++ backend/application/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url
 from django.urls import path, include
 
-urlpatterns = [
-    path('api/', include('application.api.urls')),
-]
+from django.conf.urls import url
+from django.urls import path, include
+from application.api.views import ClimateDataAPI
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'api', ClimateDataAPI, basename='api')
+urlpatterns = router.urls
