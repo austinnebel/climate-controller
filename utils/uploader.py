4cbe5488a15a1e03e5aedfd80e3a8ba708bf7faf
austinnebel <austin@true-360.com>
2021-11-11 18:13:52 -0600

    Added socket uploader class

--- utils/uploader.py
+++ utils/uploader.py
@@ -1,6 +1,9 @@
 import logging
 import requests
-
+import json
+import asyncio
+from queue import Queue
+from websockets import connect
 
 LOGGER = logging.getLogger()
 
@@ -26,3 +29,55 @@ class Database():
             return True
         LOGGER.error(f"Database returned status code of {r.status_code}. Content: {r.content}")
         return False
+
+class SocketConnector:
+
+    def __init__(self, url, user, password):
+        """
+        Utility for posting data to the local database server.
+        """
+        self.url = url
+        self.user = user
+        self.password = password
+        self.ws = None
+        self.message_queue = Queue()
+        self.loop = asyncio.get_event_loop()
+
+    async def connect(self, close_timeout = 5):
+        LOGGER.debug(f"Connecting to {self.url}")
+        try:
+            self.ws = await connect(f"ws://{self.url}", close_timeout = close_timeout)
+        except asyncio.TimeoutError as e:
+            LOGGER.error("Failed to make socket connection to database.")
+            return False
+        return True
+
+    async def send(self, message):
+        if self.ws is None:
+            connected = await self.connect()
+            if not connected:
+                return False
+
+        await self.ws.send(json.dumps(
+            {
+                "type": "receive.json",
+                "text": message
+                }
+            )
+        )
+        LOGGER.debug(f"Sent message over socket: {message}")
+        return True
+
+    async def begin_event_loop(self):
+
+        while True:
+            while not self.queue.empty():
+                await self.send(self.queue.popleft())
+            asyncio.sleep(1)
+
+    def start(self):
+        asyncio.run(self.begin_event_loop())
+
+    def send_data(self, data, timeout = 5):
+        return self.loop.run_until_complete(self.send(data))
+
